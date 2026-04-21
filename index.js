513e56738c407e3329cb2badbc0bb22ada415e7e
sgheb <rickyxk123@gmail.com>
2026-04-21 12:23:25 +0200

    variabili d'ambiente

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const dotenv = require('dotenv').config();
 
 const app = express();
 app.use(cors());
